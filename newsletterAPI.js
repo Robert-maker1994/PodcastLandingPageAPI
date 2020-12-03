31cfb1e8e48a03568d2c57b3768470845620d64d
Robert <robertpaullawson@gmail.com>
2020-12-03 13:46:47 +0000

    first commit

--- newsletterAPI.js
+++
@@ -1,8 +0,0 @@
-const app = require('express');
-const router = app.Router();
-
-const run = (res, req) => {
-    console.log('hello')
-}
-
-router.get('/newsletter', /*Controller Function*/ run );
--- server.js
+++ server.js
@@ -2,20 +2,27 @@ const express = require('express');
 const config = require('./config/app');
 const bodyParser = require("body-parser");
 const client = require('@mailchimp/mailchimp_marketing');
-const { apiKey, server, listId } = require('./config/app');
-
+const { listId } = require('./config/app');
 var cors = require('cors');
-const request = require('request');
 const md5 = require('md5');
+
 //Calling Express
 const app = express();
 
 // Basic express app setup + MiddleWare
-app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.json());
+
 app.use(cors());
 
+const corsConfig = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  }
+};
 // Setting up Console.log
 app.listen(config.port, () => { 
  console.log(
@@ -24,26 +31,20 @@ app.listen(config.port, () => {
     
       )
 });  
-
-app.post('/test', (req, res) => {
-
-  res.status(400).json()
-})
+client.setConfig({
+  apiKey: config.apiKey,
+  server: config.server,
+  listId: config.listId
+ 
+});
 
 // Testing out Calls
-app.post('/lists', async (req, res) => {
-  client.setConfig({
-    apiKey: config.apiKey,
-    server: config.server,
-    listId: config.listId
-   
-  });
+app.post( '/lists', async (req, res) => {
+   corsConfig
   try {
     const {email, firstName, lastName} = req.body;
-    console.log(req.body.email)
-
     const subscribingUser = req.body
-
+    console.log(subscribingUser)
  
    const response = await client.lists.addListMember(listId, {
     email_address: subscribingUser.email,
@@ -52,13 +53,13 @@ app.post('/lists', async (req, res) => {
       FNAME: subscribingUser.firstName,
       LNAME: subscribingUser.lastName
     }
-  });
-   res.json();
-   res.status(200)
-
+  }).then().catch((err) => console.log(err))
+  
+  return  res.redirect( 'http://localhost:3000/Success');
     }
    catch (err) {
-    console.log(err.message )
+    console.log(err.message );
+    return res.status(500).send('Server error');
 
   }
 }    );
